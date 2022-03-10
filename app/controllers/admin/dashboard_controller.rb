77d78648a11daac50023e9091df5489229f1eabd
John Chia <john.chia0106@gmail.com>
2022-03-09 18:32:06 -0800

    display products count and category count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_BASIC_AUTH_USERNAME"], password: ENV['HTTP_BASIC_AUTH_PASSWORD']
 
   def show
+    @products_count = Product.all.count
+    @category_count = Category.all.count
   end
 end
