840774f1ac6accaaa15ab4cbdaee8d54abf7700c
John Chia <john.chia0106@gmail.com>
2022-03-14 18:26:13 -0700

    refractor to use more instance variables

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,62 +5,52 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
 
     before do 
-      @category = Category.create(name:"Boats")
+      @category = Category.new
+      @product = Product.new
+      @product.name = 'test product'
+      @product.price_cents = 1;
+      @product.quantity = 10;
+      @product.category_id = @category.id;
     end
 
-    it 'Saves a valid product' do
-      boat = Product.new(
-      name: "Bokchoy",
-      price_cents: 20000,
-      quantity: 1,
-      category_id: @category.id
-      )
-      expect(boat).to be_present
+    context 'add product with correct fields' do
+      it 'Saves a valid product' do
+        @product.save
+
+        expect(@product).to be_present
+      end
     end
 
     it 'Validates name' do
-      boat = Product.new(
-      name: nil,
-      price_cents: 100,
-      quantity: 3,
-      category_id: @category.id
-      )
-      expect(boat).to_not (be_valid)
-      expect(boat.errors.full_messages).to include("Name can't be blank")
+      @product.name = nil
+      @product.save 
+
+      expect(@product).to_not (be_valid)
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end 
 
     it 'Validates price' do
-      boat = Product.new(
-      name: "Bonsai",
-      price_cents: nil,
-      quantity: 1,
-      category_id: @category.id
-      )
-      expect(boat).to_not (be_valid)
-      expect(boat.errors.full_messages).to include("Price can't be blank")
+      @product.price_cents = nil
+      @product.save 
+
+      expect(@product).to_not (be_valid)
+      expect(@product.errors.full_messages).to include("Price can't be blank")
     end 
 
     it 'Validates quantity' do
-      boat = Product.new(
-      name: "Fish",
-      price_cents: 5000,
-      quantity: nil,
-      category_id: @category.id
-      )
-      expect(boat).to_not (be_valid)
-      expect(boat.errors.full_messages).to include("Quantity can't be blank")
+      @product.quantity = nil
+      @product.save 
+      
+      expect(@product).to_not (be_valid)
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end 
 
     it 'Validates category' do
-      boat = Product.new(
-      name: "iPhone",
-      price_cents: 1000,
-      quantity: 1,
-      category_id: nil
-      )
-      expect(boat).to_not (be_valid)
-      expect(boat.errors.full_messages).to include("Category can't be blank")
-    end 
+      @product.category_id = nil
+      @product.save 
 
+      expect(@product).to_not (be_valid)
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end 
   end
 end
